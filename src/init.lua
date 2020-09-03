ef9026aa0a34804fff42bcf825db3ba984d55e12
ClockworkSquirrel <ClockworkSquirrel@users.noreply.github.com>
2020-09-03 20:10:31 +0100

    Stack overflow error
    For whatever reason, a stack overflow occurred on `self.__joinDictionary`. Moving this outside of the constructor seemed to fix this.

--- src/init.lua
+++ src/init.lua
@@ -56,44 +56,14 @@ function State.new(InitialState)
 	self.__bindables = {}
 
 	--[[
-		Helper function which creates a deep copy of passed tables.
-		It's stored inside State so that it can access self for property checks.
-
-		In v0.1.1, JoinDictionary now performs a deep copy of tables. This
-		allows nested tables within state to be modified without losing
-		original data.
-	--]]
-	self.__joinDictionary = function(...)
-		local NewDictionary = {}
-
-		for _, Dictionary in next, { ... } do
-			if (type(Dictionary) ~= "table") then
-				continue
-			end
-
-			for Key, Value in next, Dictionary do
-				if (type(Value) == "table") then
-					NewDictionary[Key] = self:__joinDictionary(NewDictionary[Key], Value)
-					continue
-				end
-
-				if self.ProtectType and NewDictionary[Key] ~= nil and typeof(NewDictionary[Key]) ~= typeof(Value) then
-					error("Attempted to set "..Key.." to new value type '"..typeof(Value).."'. Disable State.ProtectType if you want to allow this.", 2)
-				end
-				NewDictionary[Key] = Value
-			end
-		end
-
-		return NewDictionary
-	end
-
-	--[[
 		Set BasicState.Changed as an alias for BasicState.__changeEvent.Event
 	--]]
 	self.Changed = self.__changeEvent.Event
 
 	--[[
 		Set BasicState.ProtectType to default to false to allow dynamically changing state types
+
+		Type protection added by @boatbomber in v0.2.0
 	--]]
 	self.ProtectType = false
 
@@ -104,10 +74,46 @@ function State.new(InitialState)
 end
 
 --[[
+	Helper function which creates a deep copy of passed tables.
+	It's stored inside State so that it can access self for property checks.
+
+	In v0.1.1, JoinDictionary now performs a deep copy of tables. This
+	allows nested tables within state to be modified without losing
+	original data.
+--]]
+function State:__joinDictionary(...)
+	local NewDictionary = {}
+
+	for _, Dictionary in next, { ... } do
+		if (type(Dictionary) ~= "table") then
+			continue
+		end
+
+		for Key, Value in next, Dictionary do
+			if (self.ProtectType and NewDictionary[Key] and typeof(NewDictionary[Key]) ~= typeof(Value)) then
+				error(
+					string.format("attempt to set \"%s\" to new value type \"%s\". Disable State.ProtectType to allow this.", tostring(Key), typeof(Value)),
+					2
+				)
+			end
+
+			if (type(Value) == "table") then
+				NewDictionary[Key] = self:__joinDictionary(NewDictionary[Key], Value)
+				continue
+			end
+
+			NewDictionary[Key] = Value
+		end
+	end
+
+	return NewDictionary
+end
+
+--[[
 	Return a deep copy of the current stored state
 --]]
 function State:GetState()
-	return self:__joinDictionary(self.__state, {})
+	return self:__joinDictionary(self.__state)
 end
 
 --[[
@@ -124,9 +130,12 @@ end
 function State:Set(Key, Value)
 	local OldState = self:GetState()
 
-	if self.ProtectType then
-		if OldState[Key] ~= nil and typeof(Value) ~= typeof(OldState[Key]) then
-			error("Attempted to set "..Key.." to new value type '"..typeof(Value).."'. Disable State.ProtectType if you want to allow this.", 2)
+	if (self.ProtectType) then
+		if (OldState[Key] and typeof(Value) ~= typeof(OldState[Key])) then
+			error(
+				string.format("attempt to set \"%s\" to new value type \"%s\". Disable State.ProtectType to allow this.", tostring(Key), typeof(Value)),
+				2
+			)
 		end
 	end
 
